d3dbbfd9d2440077fc96bfe07048d691bca847dd
Pyae Phyo Aung (Austin) <austin.xu.me@gmail.com>
2024-12-15 00:43:08 +0700

    fix pacakge.swift target

--- Package.swift
+++ Package.swift
@@ -17,11 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "TodoMiniApp",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "TodoMiniAppTests",
-            dependencies: ["TodoMiniApp"]
+            path: "Sources"
         )
     ]
 )
