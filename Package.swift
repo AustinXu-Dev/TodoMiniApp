3adf5859edb894e84b53775441f35932c3662d51
Pyae Phyo Aung (Austin) <austin.xu.me@gmail.com>
2024-12-15 00:23:29 +0700

    fix package.swift file

--- Package.swift
+++ Package.swift
@@ -17,9 +17,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(
+        .target(
             name: "TodoMiniApp",
-            path: "./TodoMiniApp.xcframework"
+            dependencies: []
+        ),
+        .testTarget(
+            name: "TodoMiniAppTests",
+            dependencies: ["TodoMiniApp"]
         )
     ]
 )
